9969aed76d74daea76b3c7b6812e7e5e449d7c6d
rania1217 <rania2002@naver.com>
2024-01-30 23:48:49 +0900

    집에서함

--- 3_Javascript/js/연습문제7.js
+++ 3_Javascript/js/연습문제7.js
@@ -3,8 +3,10 @@ const emptyCart = document.getElementById("empty-cart");
 const total = document.getElementById("total");
 
 let result = 0;
-let clickCount = [0,0,0,0,0,0];
+let firstClick = {김밥 : 0, 라면 : 0, 튀김 : 0, 떡볶이 : 0, 돈까스 : 0, 우동 : 0};
+let clickCount = {김밥 : 0, 라면 : 0, 튀김 : 0, 떡볶이 : 0, 돈까스 : 0, 우동 : 0};
 function addToCart(menu, price) {
+
 
     cart.style.display = "flex";
     cart.style.flexDirection = "column";
@@ -51,79 +53,135 @@ function addToCart(menu, price) {
 
     switch(menu) {
         case "김밥" : row.innerText += "김밥";
-                    clickCount[0] += 1;
-                    amount.innerText = clickCount[0];break;
+                    clickCount.김밥 += 1;
+                    firstClick.김밥 += 1;
+                    if(firstClick.김밥 > 1) {
+                        container.remove();
+                        clickCount.김밥 -= 1;
+                    }
+                    amount.innerText = clickCount.김밥;break;
 
         case "라면" : row.innerText += "라면";
-                    clickCount[1] += 1;
-                    amount.innerText = clickCount[1];break;
+                    clickCount.라면 += 1;
+                    firstClick.라면 += 1;
+                    if(firstClick.라면 > 1) {
+                        container.remove();
+                        clickCount.라면 -= 1;
+                    }
+                    amount.innerText = clickCount.라면;break;
 
         case "튀김" : row.innerText += "튀김";
-                    clickCount[2] += 1;
-                    amount.innerText = clickCount[2];break;
+                    clickCount.튀김 += 1;
+                    firstClick.튀김 += 1;
+                    if(firstClick.튀김 > 1) {
+                        container.remove();
+                        clickCount.튀김 -= 1;
+                    }
+                    amount.innerText = clickCount.튀김;break;
 
         case "떡볶이" : row.innerText += "떡볶이";
-                    clickCount[3] += 1;
-                    amount.innerText = clickCount[3];break;
+                    clickCount.떡볶이 += 1;
+                    firstClick.떡볶이 += 1;
+                    if(firstClick.떡볶이 > 1) {
+                        container.remove();
+                        clickCount.떡볶이 -= 1;
+                    }
+                    amount.innerText = clickCount.떡볶이;break;
 
         case "돈까스" : row.innerText += "돈까스";
-                    clickCount[4] += 1;
-                    amount.innerText = clickCount[4];break;
+                    clickCount.돈까스 += 1;
+                    firstClick.돈까스 += 1;
+                    if(firstClick.돈까스 > 1) {
+                        container.remove();
+                        clickCount.돈까스 -= 1;
+                    }
+                    amount.innerText = clickCount.돈까스;break;
 
         case "우동" : row.innerText += "우동";
-                    clickCount[5] += 1;
-                    amount.innerText = clickCount[5];break;
-
+                    clickCount.우동 += 1;
+                    firstClick.우동 += 1;
+                    if(firstClick.우동 > 1) {
+                        container.remove();
+                        clickCount.우동 -= 1;
+                    }
+                    amount.innerText = clickCount.우동;break;
 
     }
 
-    minusBtn.addEventListener("click",()=> {
+
+
+    minusBtn.addEventListener("click",(e)=> {
         switch(menu) {
-            case "김밥" : clickCount[0] -= 1;
-                        amount.innerText = clickCount[0];break;
+            case "김밥" : clickCount.김밥 -= 1;
+                        if(clickCount.김밥 == 0){
+                            e.target.parentElement.parentElement.remove();
+                        }
+                        amount.innerText = clickCount.김밥;break;
 
-            case "라면" : clickCount[1] -= 1;
-                        amount.innerText = clickCount[1];break;
+            case "라면" : clickCount.라면 -= 1;
+                        if(clickCount.라면 == 0){
+                            e.target.parentElement.parentElement.remove();
+                        }
+                        amount.innerText = clickCount.라면;break;
 
-            case "튀김" : clickCount[2] -= 1;
-                        amount.innerText = clickCount[2];break;
+            case "튀김" : clickCount.튀김 -= 1;
+                        if(clickCount.튀김 == 0){
+                            e.target.parentElement.parentElement.remove();
+                        }
+                        amount.innerText = clickCount.튀김;break;
 
-            case "떡볶이" : clickCount[3] = 1;
-                        amount.innerText -= clickCount[3];break;
+            case "떡볶이" : clickCount.떡볶이 -= 1;
+                        if(clickCount.떡볶이 == 0){
+                            e.target.parentElement.parentElement.remove();
+                        }
+                        amount.innerText = clickCount.떡볶이;break;
 
-            case "돈까스" : clickCount[4] = 1;
-                        amount.innerText -= clickCount[4];break;
+            case "돈까스" : clickCount.돈까스 -= 1;
+                        if(clickCount.돈까스 == 0){
+                            e.target.parentElement.parentElement.remove();
+                        }
+                        amount.innerText = clickCount.돈까스;break;
 
-            case "우동" : clickCount[5] -= 1;
-                        amount.innerText = clickCount[5];break;
+            case "우동" : clickCount.우동 -= 1;
+                        if(clickCount.우동 == 0){
+                            e.target.parentElement.parentElement.remove();
+                        }
+                        amount.innerText = clickCount.우동;break;
 
         }
-        result = 3000*clickCount[0]+4500*clickCount[1]+5000*clickCount[2]+6000*clickCount[3]+7500*clickCount[4]+5000*clickCount[5]
+        if(cart.childElementCount == 0){
+            cart.style.display = "none";
+            emptyCart.style.display = "flex";
+            emptyCart.style.justifyContent = "center";
+            emptyCart.innerHTML = "장바구니가 비어 있습니다.";
+        }
+
+        result = 3000*clickCount.김밥+4500*clickCount.라면+5000*clickCount.튀김+6000*clickCount.떡볶이+7500*clickCount.돈까스+5000*clickCount.우동
         total.innerText = `합계: ₩${result}`;
     });
 
     plusBtn.addEventListener("click",()=> {
         switch(menu) {
-            case "김밥" : clickCount[0] += 1;
-                        amount.innerText = clickCount[0];break;
+            case "김밥" : clickCount.김밥 += 1;
+                        amount.innerText = clickCount.김밥;break;
 
-            case "라면" : clickCount[1] += 1;
-                        amount.innerText = clickCount[1];break;
+            case "라면" : clickCount.라면 += 1;
+                        amount.innerText = clickCount.라면;break;
 
-            case "튀김" : clickCount[2] += 1;
-                        amount.innerText = clickCount[2];break;
+            case "튀김" : clickCount.튀김 += 1;
+                        amount.innerText = clickCount.튀김;break;
  
-            case "떡볶이" : clickCount[3] = 1;
-                        amount.innerText += clickCount[3];break;
+            case "떡볶이" : clickCount.떡볶이 += 1;
+                        amount.innerText = clickCount.떡볶이;break;
 
-            case "돈까스" : clickCount[4] = 1;
-                        amount.innerText += clickCount[4];break;
+            case "돈까스" : clickCount.돈까스 += 1;
+                        amount.innerText = clickCount.돈까스;break;
 
-            case "우동" : clickCount[5] += 1;
-                        amount.innerText = clickCount[5];break;
+            case "우동" : clickCount.우동 += 1;
+                        amount.innerText = clickCount.우동;break;
 
         }
-        result = 3000*clickCount[0]+4500*clickCount[1]+5000*clickCount[2]+6000*clickCount[3]+7500*clickCount[4]+5000*clickCount[5]
+        result = 3000*clickCount.김밥+4500*clickCount.라면+5000*clickCount.튀김+6000*clickCount.떡볶이+7500*clickCount.돈까스+5000*clickCount.우동
         total.innerText = `합계: ₩${result}`;
     });
 
@@ -132,10 +190,25 @@ function addToCart(menu, price) {
     remove.addEventListener("click", e => {
         const parent = e.target.parentElement;
         parent.remove();
-        result = 3000*clickCount[0]+4500*clickCount[1]+5000*clickCount[2]+6000*clickCount[3]+7500*clickCount[4]+5000*clickCount[5]
+        switch(menu) {
+            case "김밥" : clickCount.김밥 = 0;break;
+            case "라면" : clickCount.라면 = 0;break;
+            case "튀김" : clickCount.튀김 = 0;break;
+            case "떡볶이" : clickCount.떡볶이 = 0;break;
+            case "돈까스" : clickCount.돈까스 = 0;break;
+            case "우동" : clickCount.우동 = 0;break;
+        }
+        result = 3000*clickCount.김밥+4500*clickCount.라면+5000*clickCount.튀김+6000*clickCount.떡볶이+7500*clickCount.돈까스+5000*clickCount.우동
         total.innerText = `합계: ₩${result}`;
+
+        if(cart.childElementCount == 0){
+            cart.style.display = "none";
+            emptyCart.style.display = "flex";
+            emptyCart.style.justifyContent = "center";
+            emptyCart.innerHTML = "장바구니가 비어 있습니다.";
+        }
     });
-    result = 3000*clickCount[0]+4500*clickCount[1]+5000*clickCount[2]+6000*clickCount[3]+7500*clickCount[4]+5000*clickCount[5]
+    result = 3000*clickCount.김밥+4500*clickCount.라면+5000*clickCount.튀김+6000*clickCount.떡볶이+7500*clickCount.돈까스+5000*clickCount.우동
     
     total.innerText = `합계: ₩${result}`;
 }
